a1bbdc6a9dbaec8cd89ac3a64af83d7922581ef4
MartelAxe <martelaxe@gmail.com>
2022-11-06 11:11:47 -0600

    final version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const goldListABI = require("./goldListInterface.json") // use the require metho
 
 const provider = new ethers.providers.JsonRpcProvider(
 
-  `https://polygon-mumbai.g.alchemy.com/v2/${process.env['ALCHEMY_API_KEY']}` 
+  `https://polygon-mumbai.g.alchemy.com/v2/${process.env['ALCHEMY_API_KEY']}`
 );
 
 
@@ -24,51 +24,54 @@ const orbis = new Orbis();
 
 const GoldListContract = new ethers.Contract(process.env['CONTRACT_ADDRESS'], goldListABI, connectedSigner);
 
-exports.insertNewWhiteList = async (req, res) => {
+export async function insertWhiteList(req, res) {
   try {
 
-      // Api test
+    // Api test
 
 
-      console.log(`Geting data from ${process.env.DID}`);
-      const profile = await orbis.getProfile(process.env.DID);
-      let data = profile.data.details.profile.data;
-      console.log(data)
-      let newAddresses = Object.keys(data);
+    console.log(`Geting data from ${process.env.DID}`);
+    const profile = await orbis.getProfile(process.env.DID);
+    let data = profile.data.details.profile.data;
+    console.log(data)
+    let newAddresses = Object.keys(data);
 
-      newAddresses = [...new Set(newAddresses)]
-
-      //
-
-      let actualWhiteList = await GoldListContract.getGoldMembers();
-
-      actualWhiteList = actualWhiteList.map((address) => address.toLowerCase());
-
-      // check uniques and make it a Set
-      const actualWhiteLisUnique = new Set([...new Set(actualWhiteList)]);
+    newAddresses = newAddresses.map((address) => address.toLowerCase())
 
 
+    newAddresses = [...new Set(newAddresses)]
 
-      console.log(actualWhiteLisUnique);
+    //
 
-      const addressesToInsert = newAddresses.filter((address => {
-          return !actualWhiteLisUnique.has(address);
+    let actualWhiteList = await GoldListContract.getGoldMembers();
 
-      }))
+    actualWhiteList = actualWhiteList.map((address) => address.toLowerCase());
 
-      console.log("Addresses to Insert", addressesToInsert);
+    // check uniques and make it a Set
+    const actualWhiteLisUnique = new Set([...new Set(actualWhiteList)]);
 
-      const trueList = addressesToInsert.map(address => true);
 
-      // console.log(trueList);
 
-      if (addressesToInsert.length != 0) {
-          const tx = await GoldListContract.addBatchGoldList(addressesToInsert, trueList);
-      }
-      res.status(200).send("Sucessfull");
+    console.log(actualWhiteLisUnique);
+
+    const addressesToInsert = newAddresses.filter((address => {
+      return !actualWhiteLisUnique.has(address);
+
+    }))
+
+    console.log("Addresses to Insert", addressesToInsert);
+
+    const trueList = addressesToInsert.map(address => true);
+
+    // console.log(trueList);
+
+    if (addressesToInsert.length != 0) {
+      const tx = await GoldListContract.addBatchGoldList(addressesToInsert, trueList);
+    }
+    res.status(200).send("Sucessfull");
 
   } catch (error) {
     res.status(500).send(error.message);
-}
+  }
 }
 
